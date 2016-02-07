655cdaf436f6dfe52227569512fbf70e5c557679
Mike Lin <mlin@mlin.net>
2016-02-07 12:12:52 -0800

    client skeleton

---
+++ client/bamnexus.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2.7
+
+import subprocess
+import argparse
+import requests
+import sys
+
+DEFAULT_SERVER='http://localhost:48444'
+
+def query_bamnexus(namespace, accession, server=DEFAULT_SERVER):
+    query_url = '/'.join([args.server, 'bam', args.namespace, args.accession])
+    response = requests.get(query_url)
+    if response.status_code != 200:
+        print >> sys.stderr, ("Error: HTTP status " + str(response.status_code))
+        print >> sys.stderr, response.json()
+        sys.exit(1)
+    return response.json()
+
+parser = argparse.ArgumentParser(description='BAMnexus streaming client')
+parser.add_argument('--server', metavar='URL', type=str, default=DEFAULT_SERVER, help='BAMnexus server endpoint')
+parser.add_argument('namespace', type=str, help="accession namespace")
+parser.add_argument('accession', type=str, help="BAM accession")
+args = parser.parse_args()
+
+bam_info = query_bamnexus(args.namespace, args.accession, server=args.server)
+
+# TODO: print the bamHeaderBGZF iff the byte range beginning is non-zero
+# TODO: handle bam_info.httpRequestHeaders
+
+subprocess.check_call(['curl','-LSs',bam_info['url']])
+
+# TODO: print the EOF marker which consists of the following hexadecimal bytes:
+# 1f 8b 08 04 00 00 00 00 00 ff 06 00 42 43 02 00 1b 00 03 00 00 00 00 00 00 00 00 00
