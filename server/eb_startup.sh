450e6c2efc9eee6bc17c19ee0314d75009391f37
Mike Lin <mlin@mlin.net>
2016-02-11 00:24:02 -0800

    Eb: store database on host

--- server/eb_startup.sh
+++ server/eb_startup.sh
@@ -6,7 +6,7 @@ HERE="${BASH_SOURCE[0]}"
 while [ -h "$HERE" ] ; do HERE="$(readlink "$HERE")"; done
 HERE="$( cd -P "$( dirname "$HERE" )" && pwd )"
 
-mkdir /data
-aws s3 cp s3://dnanexus-rnd-htsnexus/current.db.gz /data
-gunzip /data/current.db.gz
-$HERE/server.sh --bind 0.0.0.0 /data/current.db
+mkdir /host_var/htsnexus
+aws s3 cp s3://dnanexus-rnd-htsnexus/current.db.gz /host_var/htsnexus
+gunzip /host_var/htsnexus/current.db.gz
+$HERE/server.sh --bind 0.0.0.0 /host_var/htsnexus/current.db
