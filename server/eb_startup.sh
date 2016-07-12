9763db2aa2d303fdf76d5b0ea5a82fa7694febd5
Mike Lin <mlin@mlin.net>
2016-07-12 10:23:14 -0400

    eb_startup.sh: decompress the database in streaming fashion

--- server/eb_startup.sh
+++ server/eb_startup.sh
@@ -7,6 +7,5 @@ while [ -h "$HERE" ] ; do HERE="$(readlink "$HERE")"; done
 HERE="$( cd -P "$( dirname "$HERE" )" && pwd )"
 
 mkdir /host_var/htsnexus
-aws s3 cp s3://dnanexus-rnd-htsnexus/current.db.gz /host_var/htsnexus
-gunzip /host_var/htsnexus/current.db.gz
+aws s3 cp s3://dnanexus-rnd-htsnexus/current.db.gz - | pigz -dc > /host_var/htsnexus/current.db
 $HERE/server.sh --bind 0.0.0.0 /host_var/htsnexus/current.db
