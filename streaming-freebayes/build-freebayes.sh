c4251801d9a0e60470d0974b8735a19959f5311e
Mike Lin <mlin@mlin.net>
2016-09-09 16:50:13 -0700

    wip

---
+++ streaming-freebayes/build-freebayes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex -o pipefail
+export FREEBAYES_REVISION=v1.0.2
+docker run -t -i --rm \
+  -v `pwd`:/io \
+  ubuntu:trusty \
+  bash -c "set -ex -o pipefail
+           apt-get -qq update
+           DEBIAN_FRONTEND=noninteractive apt-get -qq install -y --no-install-recommends --no-install-suggests ca-certificates make cmake git g++ zlib1g-dev
+           git clone -b ${FREEBAYES_REVISION} --recursive https://github.com/ekg/freebayes.git
+           make -j $(nproc) -C freebayes
+           cp freebayes/bin/freebayes freebayes/vcflib/bin/vcffirstheader /io"
---
+++ streaming-freebayes/main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ex -o pipefail
+#htsnexus -v -r 21 BroadHiSeqX_b37 NA12878
+freebayes
