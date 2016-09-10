7aafb93b1dac12db4143ecf0a2daa1696128e851
Mike Lin <mlin@mlin.net>
2016-09-10 09:50:34 -0700

    reorganize resources

--- streaming-freebayes/main.sh
+++ streaming-freebayes/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # main entry point inside the ga4gh-streaming-freebayes container
-
+# TODO: multiple accessions; 21 only
 if [ $# -ne 3 ]; then
   echo "Usage: docker run dnamlin/ga4gh-streaming-freebayes SERVER NAMESPACE ID"
   exit 1
@@ -12,11 +12,12 @@ fi
 
 set -ex -o pipefail
 
-# fetch and decompress reference genome
+# fetch, unpack, and index reference genome
 aria2c -q -x 10 -j 10 -s 10 --retry-wait=1 https://s3.amazonaws.com/1000genomes/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
 pigz -d hs37d5.fa.gz || true # hs37d5.fa.gz has trailing junk which causes a nonzero exit of pigz
+samtools faidx hs37d5.fa
 
-# parallelize calling subjob.h on each genomic region
-(parallel -k -j $(expr `nproc` \* 3 / 2) --halt 2 -a hs37d5_interLCR_intervals.4Mbp.regions ./subjob.sh "$1" "$2" "$3" {}) | vcffirstheader
+# parallelize calling subjob.sh on each genomic region
+(parallel -k -j $(expr `nproc` \* 5 / 4) --halt 2 -a hs37d5_interLCR_intervals.4Mbp.regions ./subjob.sh "$1" "$2" "$3" {}) | vcffirstheader
 
 kill $dstat_pid
--- streaming-freebayes/subjob.sh
+++ streaming-freebayes/subjob.sh
@@ -3,11 +3,10 @@
 # script to run variant calling in ONE region (used by parallel in main.sh)
 
 bamfn=$(mktemp XXXXXXXXX.bam)
-# TODO: multiple accessions
 # TODO: retry (just use parallel --retry, with 1 input line?)
 >&2 echo "$4"
 htsnexus -r "$4" -s "$1" "$2" "$3" > $bamfn
-bamtools index -in $bamfn # just to suppress an annoying stderr warning from freebayes
+samtools index $bamfn # just to suppress an annoying stderr warning from freebayes
 freebayes -f hs37d5.fa --standard-filters --min-repeat-entropy 1 --no-partial-observations --min-alternate-fraction 0.1 --region "$4" $bamfn
 
 rm $bamfn
