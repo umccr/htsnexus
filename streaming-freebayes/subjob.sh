b4cc20b12d005a7afc5e97b6557e6e5aac53058a
Mike Lin <mlin@mlin.net>
2016-09-11 07:55:02 -0700

    retries

--- streaming-freebayes/subjob.sh
+++ streaming-freebayes/subjob.sh
@@ -4,34 +4,41 @@
 
 JOB="$1"
 JOBS="$2"
-REGION="$3"
-SERVER="$4"
-NAMESPACE="$5"
+export REGION="$3"
+export SERVER="$4"
+export NAMESPACE="$5"
 shift 5
 
 set -e -o pipefail
 
-# for each sample/"read group set" ID
+# For each sample/"read group set" ID, use the htsnexus client to fetch a BAM
+# slice covering the desired region.
+fetch() {
+    # this subroutine is invoked below by parallel which is used here just for
+    # retry logic.
+    htsnexus -r "$REGION" -s "$SERVER" "$NAMESPACE" "$1" > "$2"
+}
+export -f fetch
 SECONDS=0
 bams=()
 for id in "$@"; do
-    # Fetch a BAM slice covering the desired region
-    # TODO: timeout+retry (just use parallel --retry, with 1 input line?)
     bamfn=$(mktemp "${REGION}-${id}-XXXXXX.bam")
-    htsnexus -r "$REGION" -s "$SERVER" "$NAMESPACE" "$id" > $bamfn
-    bams+=($bamfn)
-    # index the BAM slice -- this is only to suppress an annoying stderr
-    # warning from freebayes
-    samtools index $bamfn &
+    # use parallel to call fetch with retry
+    parallel --retries 3 fetch "$id" "$bamfn" ::: 0
+    bams+=("$bamfn")
+    # index the BAM slice -- this is unnecessary except to suppress an annoying
+    # stderr warning from freebayes
+    samtools index "$bamfn" &
 done
 T_FETCHING=$SECONDS
 wait
+bams_size=$(du -ch --apparent-size ${bams[@]} | tail -n1 | cut -f1)
 
 # run freebayes on the BAM slices
 SECONDS=0
 freebayes --standard-filters --min-repeat-entropy 1 --no-partial-observations --min-alternate-fraction 0.1 \
     -f hs37d5.fa --region "$REGION" ${bams[@]}
->&2 echo "(${JOB}/${JOBS}) $REGION fetched in ${T_FETCHING}s, called in ${SECONDS}s"
+>&2 echo "(${JOB}/${JOBS}) $REGION fetched ${bams_size} in ${T_FETCHING}s, freebayes took ${SECONDS}s"
 
 rm ${bams[@]}
 
