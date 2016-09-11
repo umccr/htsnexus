4baa0db11ddcd3b27ec410d92bcfcf22f7470266
Mike Lin <mlin@mlin.net>
2016-09-10 17:12:25 -0700

    option to call chromosome 22 only

--- streaming-freebayes/subjob.sh
+++ streaming-freebayes/subjob.sh
@@ -2,15 +2,17 @@
 #
 # script to run variant calling in ONE region (used by parallel in main.sh)
 
-REGION="$1"
-SERVER="$2"
-NAMESPACE="$3"
-shift 3
->&2 echo "$REGION"
+JOB="$1"
+JOBS="$2"
+REGION="$3"
+SERVER="$4"
+NAMESPACE="$5"
+shift 5
 
 set -e -o pipefail
 
 # for each sample/"read group set" ID
+SECONDS=0
 bams=()
 for id in "$@"; do
     # Fetch a BAM slice covering the desired region
@@ -22,10 +24,14 @@ for id in "$@"; do
     # warning from freebayes
     samtools index $bamfn &
 done
+T_FETCHING=$SECONDS
 wait
 
 # run freebayes on the BAM slices
+SECONDS=0
 freebayes --standard-filters --min-repeat-entropy 1 --no-partial-observations --min-alternate-fraction 0.1 \
     -f hs37d5.fa --region "$REGION" ${bams[@]}
+>&2 echo "(${JOB}/${JOBS}) $REGION fetched in ${T_FETCHING}s, called in ${SECONDS}s"
 
 rm ${bams[@]}
+
