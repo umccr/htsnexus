0d019ec4630740675b203509befa08ebf9ebbe1c
Mike Lin <mlin@mlin.net>
2016-09-13 14:01:07 -0700

    polishing

--- streaming-freebayes/resources/app/main.sh
+++ streaming-freebayes/resources/app/main.sh
@@ -8,16 +8,16 @@ set -e -o pipefail
 # process command line #
 ########################
 USAGE="Usage:
-  docker run --rm dnamlin/ga4gh-streaming-freebayes [-t] SERVER_URL NAMESPACE ID [ID2 ID3 ...] > variants.vcf
+  docker run --rm dnamlin/ga4gh-streaming-freebayes [-c CHR] SERVER_URL NAMESPACE ID [ID2 ID3 ...] > variants.vcf
 Options:
-  -t    test mode; process chromosome 22 only
+  -c CHR    analyze one chromosome only (e.g. 22)
 "
 
-only22=""
-while getopts "t" opt; do
+CHR=""
+while getopts "c:" opt; do
   case $opt in
-    t)
-      only22="true"
+    c)
+      CHR="$OPTARG"
       ;;
     *)
       >&2 echo "$USAGE"
@@ -36,6 +36,15 @@ SERVER="$1"
 NAMESPACE="$2"
 shift 2 # now $@ holds the IDs
 
+if [ -n "$CHR" ]; then
+  if ! grep "^${CHR}:" hs37d5_interLCR_intervals.4Mbp.regions > regions; then
+    >&2 echo "Unknown chromosome ${CHR}; try one of: $(cut -d ':' -f1 hs37d5_interLCR_intervals.4Mbp.regions | sort -V | uniq | tr '\n' ' ')"
+    exit 1
+  fi
+else
+  mv hs37d5_interLCR_intervals.4Mbp.regions regions
+fi
+
 # log system utilization statistics to stderr
 >&2 dstat --nocolor -cmdn 60 &
 trap '[ -n "$(jobs -pr)" ] && kill $(jobs -pr)' INT QUIT TERM EXIT
@@ -43,7 +52,7 @@ trap '[ -n "$(jobs -pr)" ] && kill $(jobs -pr)' INT QUIT TERM EXIT
 #############################################
 # fetch, unpack, and index reference genome #
 #############################################
->&2 echo "* Preparing reference genome *"
+>&2 echo "*** Preparing reference genome"
 aria2c -q -x 10 -j 10 -s 10 --retry-wait=1 https://s3.amazonaws.com/1000genomes/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
 pigz -d hs37d5.fa.gz || true # hs37d5.fa.gz has trailing junk which causes a nonzero exit of pigz
 samtools faidx hs37d5.fa
@@ -52,15 +61,14 @@ samtools faidx hs37d5.fa
 # parallelize calling subjob.sh on each genomic region. Emits VCF to stdout.  #
 # The post-processing pipeline is adapted from the freebayes-parallel script. #
 ###############################################################################
->&2 echo "* Beginning variant calling *"
-if [ -n "$only22" ]; then
-  grep "^22" hs37d5_interLCR_intervals.4Mbp.regions > regions
-else
-  mv hs37d5_interLCR_intervals.4Mbp.regions regions
-fi
 JOBS=$(cat regions | wc -l)
+if [ -n "$CHR" ]; then
+  >&2 echo "*** Beginning variant calling for chromosome ${CHR} (${JOBS} jobs)"
+else
+  >&2 echo "*** Beginning whole-genome variant calling (${JOBS} jobs)"
+fi
 SECONDS=0
 (parallel -k -j $(expr `nproc` \* 5 / 4) --halt 2 --delay 1 -a regions \
     ./subjob.sh {#} "$JOBS" {} "$SERVER" "$NAMESPACE" $@) \
   | vcffirstheader | vcfstreamsort -w 1000 | vcfuniq
->&2 echo "* Variant calling completed in ${SECONDS}s *"
+>&2 echo "*** Variant calling completed in ${SECONDS}s"
--- streaming-freebayes/resources/app/subjob.sh
+++ streaming-freebayes/resources/app/subjob.sh
@@ -9,14 +9,14 @@ export SERVER="$4"
 export NAMESPACE="$5"
 shift 5
 
+source with_backoff.sh
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.1
 set -e -o pipefail
 
 # For each sample/"read group set" ID, use the htsnexus client to fetch a BAM
 # slice covering the desired region.
 fetch() {
-    # this subroutine is invoked below by parallel which is used here just for
-    # retry logic.
+    # this subroutine is invoked below within retry logic
     htsnexus -r "$REGION" -s "$SERVER" "$NAMESPACE" "$1" > "$2"
 }
 export -f fetch
@@ -24,8 +24,8 @@ SECONDS=0
 bams=()
 for id in "$@"; do
     bamfn=$(mktemp "${REGION}-${id}-XXXXXX.bam")
-    # use parallel to call fetch with retry
-    parallel --retries 5 fetch "$id" "$bamfn" ::: 0
+    # call fetch with retry
+    with_backoff fetch "$id" "$bamfn"
     bams+=("$bamfn")
     # index the BAM slice -- this is unnecessary except to suppress an annoying
     # stderr warning from freebayes
@@ -37,7 +37,7 @@ bams_size=$(du -ch --apparent-size ${bams[@]} | tail -n1 | cut -f1)
 
 # run freebayes on the BAM slices
 SECONDS=0
-freebayes --standard-filters --min-repeat-entropy 1 --no-partial-observations --min-alternate-fraction 0.1 \
+freebayes --use-best-n-alleles=6 --standard-filters --min-repeat-entropy 1 --no-partial-observations --min-alternate-fraction 0.1 \
     -f hs37d5.fa --region "$REGION" ${bams[@]}
 >&2 echo "(${JOB}/${JOBS}) $REGION fetched ${bams_size} in ${T_FETCHING}s, freebayes ran ${SECONDS}s"
 
