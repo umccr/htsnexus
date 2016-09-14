8651f030486693f2ab09f17eeaadb1b963f6d821
Mike Lin <mlin@mlin.net>
2016-09-14 00:11:45 -0700

    reorg

---
+++ examples/streaming-freebayes/resources/app/with_backoff.sh
@@ -0,0 +1,39 @@
+# http://stackoverflow.com/a/8351489
+
+# Retries a command a configurable number of times with backoff.
+#
+# The retry count is given by ATTEMPTS (default 5), the initial backoff
+# timeout is given by TIMEOUT in seconds (default 1.)
+#
+# Successive backoffs double the timeout.
+function with_backoff {
+  local max_attempts=${ATTEMPTS-5}
+  local timeout=${TIMEOUT-1}
+  local attempt=0
+  local exitCode=0
+
+  while (( $attempt < $max_attempts ))
+  do
+    set +e
+    "$@"
+    exitCode=$?
+    set -e
+
+    if [[ $exitCode == 0 ]]
+    then
+      break
+    fi
+
+    echo "Retrying in ${timeout}: $@" 1>&2
+    sleep $timeout
+    attempt=$(( attempt + 1 ))
+    timeout=$(( timeout * 2 ))
+  done
+
+  if [[ $exitCode != 0 ]]
+  then
+    echo "You've failed me for the last time! $@" 1>&2
+  fi
+
+  return $exitCode
+}
